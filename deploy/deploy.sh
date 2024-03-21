2d1f841a2438b7aa070341f6dea0f695fb9d38bc
Lucas Choque <lucas@openix.com.ar>
2024-03-21 15:59:11 -0300

    add deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker_file_path=/home/ubuntu/docker-compose.yml
+
+cat << EOF > $docker_file_path
+version: '3.4'
+
+services:
+  web-app:
+    container_name: web-app
+    image: ${IMAGE}
+    ports:
+      - 80:80
+    environment:
+      - VITE_TOQUI_JIRA_REPORT_API=${VITE_TOQUI_JIRA_REPORT_API}
+EOF
+
+echo "$REGISTRY_PASSWORD_RO" | docker login -u "$REGISTRY_USERNAME" --password-stdin
+docker system prune -af
+docker compose up -d
